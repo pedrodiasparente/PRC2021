85fd1758e8f163e88f4cabfab6be9a14ee019840
pedrodiasparente <pdparente@gmail.com>
2021-03-19 16:05:31 +0000

    tpc feito

--- TP4/tabPer/routes/index.js
+++ TP4/tabPer/routes/index.js
@@ -27,7 +27,7 @@ var getLink = "http://www.localhost:7200/repositories/PRC-tpc4-Tab-Periodica" + 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
 
   var query = `SELECT * WHERE { ?s ?p el:Element }`
   var encoded = encodeURIComponent(prefixes + query)
@@ -85,5 +85,101 @@ router.get('/', function(req, res, next) {
 
     .catch(erro => console.log(erro))
 });
+*/
+router.get('/', function(req, res, next) {
 
+  res.render('index')
+});
+
+router.get('/elems', function(req, res, next) {
+  var query = `SELECT * WHERE { ?s ?p el:Element } order by asc(?s)`
+  var encoded = encodeURIComponent(prefixes + query)
+
+  axios.get(getLink + encoded)
+
+    .then(dados => {
+
+        var props = dados.data.results.bindings.map(bind => bind.s.value.split('#')[1] )
+
+        var elems = []
+        props.forEach(a => {
+          if (a != null) elems.push(a)
+        })
+        console.log(elems)
+
+        res.render('elementos', {elems: elems})
+      })
+
+    .catch(erro => console.log(erro))
+});
+
+router.get('/elems/:e', function(req, res, next) {
+  var query = `SELECT * WHERE { el:` + req.params.e + ` ?p ?o }`
+  var encoded = encodeURIComponent(prefixes + query)
+
+  axios.get(getLink + encoded)
+
+    .then(dados => {
+
+      var props = dados.data.results.bindings.map(bind => {return({
+          p: bind.p.value.split('#')[1],
+          o: (bind.o.type == 'literal') ? bind.o.value : bind.o.value.split('#')[1]
+        })
+      })
+
+        var elems = []
+        props.forEach(a => {
+          if (a != null) elems.push(a)
+        })
+        console.log(elems)
+
+        res.render('elemento', {data: elems})
+      })
+
+    .catch(erro => console.log(erro))
+});
+
+router.get('/grupos', function(req, res, next) {
+  var query = `SELECT * WHERE { ?s ?p el:Group } order by asc(?s)`
+  var encoded = encodeURIComponent(prefixes + query)
+
+  axios.get(getLink + encoded)
+
+    .then(dados => {
+
+        var props = dados.data.results.bindings.map(bind => bind.s.value.split('#')[1] )
+
+        var grupos = []
+        props.forEach(a => {
+          if (a != null) grupos.push(a)
+        })
+        console.log(grupos)
+
+        res.render('grupos', {grupos: grupos})
+      })
+
+    .catch(erro => console.log(erro))
+});
+
+router.get('/periodos', function(req, res, next) {
+  var query = `SELECT * WHERE { ?s ?p el:Period } order by asc(?s)`
+  var encoded = encodeURIComponent(prefixes + query)
+
+  axios.get(getLink + encoded)
+
+    .then(dados => {
+
+        var props = dados.data.results.bindings.map(bind => bind.s.value.split('#')[1] )
+
+        var periodos = []
+        props.forEach(a => {
+          if (a != null) periodos.push(a)
+        })
+        console.log(periodos)
+
+        res.render('periodos', {periodos: periodos})
+      })
+
+    .catch(erro => console.log(erro))
+});
 module.exports = router;
